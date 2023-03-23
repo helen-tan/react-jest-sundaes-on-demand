c1a4af27c673a561e27d02d0af5bdd24909b904a
helen-tan <helentanmz0909@gmail.com>
2023-03-23 16:14:57 +0800

    Use async and findAllByRole instead in tests, bcos elements appear asynchronously

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,13 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
-test("Displays image for each scoop option from server", () => {
+test("Displays image for each scoop option from server", async () => {
     render(<Options optionType="scoops"/>)
 
     // Find images
-    const scoopImages = screen.getAllByRole('img', { name: /scoop$/i })
-    // Expecting 2 results
-    expect(scoopImages).toHaveLength(2)
+    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i })
+    expect(scoopImages).toHaveLength(2) // Expecting 2 results
+
     // Confirm alt text of images
     const altText = scoopImages.map((element) => element.alt)
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']) // Arrays, Objects -> use .toEqual() matcher. Numbers, Strings -> .toBe() 
