cac0c41f51fb57b89f823d7416135db225d6414e
helen-tan <helentanmz0909@gmail.com>
2023-03-23 14:28:42 +0800

    Mock Service Worker - Configure create-react-app so that MSW will intercept network requests and return res from handlers

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// src/setupTests.js
+import { server } from './mocks/server.js'
+// Establish API mocking before all tests.
+beforeAll(() => server.listen()) // i.e, any network request that comes through, route them to Mock Service Worker instead of the actual network
+
+// After each test, Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+
+// Close server/ Clean up after tests are finished. 
+afterAll(() => server.close())
