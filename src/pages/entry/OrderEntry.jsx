5909e642b9b5bc59e33a904d5dd6987a74968d93
helen-tan <helentanmz0909@gmail.com>
2023-03-28 17:24:09 +0800

    Wrote tests for Grand Total + passing react code

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,16 @@
 import Options from "./Options"
+import { useOrderDetailsContext } from "../../contexts/OrderDetailsContext"
+import { formatCurrency } from "../../utilities"
 
 export default function OrderEntry() {
+    const { totals } = useOrderDetailsContext()
+
     return (
         <div>
+            <h1>Design Your Sundae!</h1>
             <Options optionType='scoops' />
             <Options optionType='toppings' />
+            <h2>Grand total: { formatCurrency(totals.scoops + totals.toppings) }</h2>
         </div>
     )
 }
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -2,11 +2,13 @@ import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event" 
 import Options from "../Options";
 import { logRoles } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
 
 test("Updates scoop subtotal when scoops change", async () => {
     const user = userEvent.setup()
 
-    const { container } = render(<Options optionType="scoops" />)
+    // const { container } = render(<Options optionType="scoops" />)
+    render(<Options optionType="scoops" />)
     
     // Total to start at $0.00
     const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false })
@@ -22,7 +24,7 @@ test("Updates scoop subtotal when scoops change", async () => {
     
     // Update chocolate scoops to 2 (via input), and check the subtotal
     const chocolateInput = await screen.findByRole("spinbutton", { name: "Chocolate" })
-    logRoles(container)
+    //logRoles(container)
 
     await user.clear(chocolateInput)
     await user.type(chocolateInput, '2')     
@@ -57,5 +59,82 @@ test("Updates toppings subtotal when toppings change", async () => {
     await user.click(mandMsCheckbox)
     expect(mandMsCheckbox).not.toBeChecked()
     expect(toppingsSubtotal).toHaveTextContent("1.50")
+})
 
+describe("Grand total tests", () => {
+    test("Grand total starts at $0.00", () => {
+        render(<OrderEntry />)
+
+        const grandTotal = screen.getByRole("heading", { name: /grand total: \$/i })
+        expect(grandTotal).toHaveTextContent("0.00")
+    })
+
+    test("Grand total updates properly if scoop is added first", async () => { // async bcos 1. we are awaiting data from axios and 2. we are using userEvent
+        const user = userEvent.setup()
+
+        render(<OrderEntry />)
+
+        const grandTotal = screen.getByRole("heading", { name: /grand total: \$/i })
+        const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" })
+
+        // Update vanilla scoops to 1 (via input)
+        await user.clear(vanillaInput)             // Clear input
+        await user.type(vanillaInput, "2")         // Type 1 into the input
+
+        expect(grandTotal).toHaveTextContent("4.00")
+
+        // Add Cherries and check grand total again
+        const cherriesCheckbox = await screen.findByRole("checkbox", { name: "Cherries" })
+
+        await user.click(cherriesCheckbox)
+        expect(grandTotal).toHaveTextContent("5.50")
+    })
+
+    test("Grand total updates properly if topping is added first", async () => {
+        const user = userEvent.setup()
+
+        render(<OrderEntry />)
+        
+        const grandTotal = screen.getByRole("heading", { name: /grand total: \$/i })
+        const cherriesCheckbox = await screen.findByRole("checkbox", { name: "Cherries" })
+
+        // Add Cherries and check grand total
+        await user.click(cherriesCheckbox)
+        expect(grandTotal).toHaveTextContent("1.50")
+
+        // Update vanilla scoops to 2 and check grand total
+        const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" })
+
+        await user.clear(vanillaInput)
+        await user.type(vanillaInput, "2")
+
+        expect(grandTotal).toHaveTextContent("5.50")
+    })
+
+    test("Grand total updates properly if an item is removed", async () => {
+        const user = userEvent.setup()
+
+        render(<OrderEntry />)
+
+        const grandTotal = screen.getByRole("heading", { name: /grand total: \$/i })
+        const cherriesCheckbox = await screen.findByRole("checkbox", { name: "Cherries" })
+        const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" })
+
+        // Add cherries
+        await user.click(cherriesCheckbox) // No need to assert after bcos the previous test would have done that
+
+        // Update Vanilla scoops to 2
+        await user.clear(vanillaInput)
+        await user.type(vanillaInput, "2") // No need to assert after bcos the previous test would have done that
+
+        // Remove 1 scoop of vanilla
+        await user.clear(vanillaInput)
+        await user.type(vanillaInput, "1")
+
+        expect(grandTotal).toHaveTextContent("3.50")
+
+        // Remove cherries and check grand total
+        await user.click(cherriesCheckbox)
+        expect(grandTotal).toHaveTextContent("2.00")
+    })
 })
