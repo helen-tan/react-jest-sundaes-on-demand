005dc08b0067fe82ffe400623f9051aeed4727b4
helen-tan <helentanmz0909@gmail.com>
2023-03-28 10:06:15 +0800

    Resolved spinbutton bug

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -8,22 +8,22 @@ test("Updates scoop subtotal when scoops change", async () => {
     const user = userEvent.setup()
 
     const { container } = render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider })
-    logRoles(container)
-
+    
     // Total to start at $0.00
     const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false })
     expect(scoopsSubtotal).toHaveTextContent('0.00')
-
+    
     // Update vanilla scoops to 1 (via input), and check the subtotal
-    const vanillaInput = await screen.findByRole('spinButton', { name: 'Vanilla' })  // Need to use findByxxx (for async stuff) as it is not going to populate until it gets options from the server
-
+    const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' })  // Need to use findByxxx (for async stuff) as it is not going to populate until it gets options from the server
+    
     await user.clear(vanillaInput)      // Clear the input first (await to make sure it finishes before moving on)
     await user.type(vanillaInput, '1')  // Type into the element (Even if is number input, we need to provide a string)
-
+    
     expect(scoopsSubtotal).toHaveTextContent("2.00") // Each scoop is $2
-
+    
     // Update chocolate scoops to 2 (via input), and check the subtotal
-    const chocolateInput = await screen.findByRole("spinButton", { name: "Chocolate" })
+    const chocolateInput = await screen.findByRole("spinbutton", { name: "Chocolate" })
+    logRoles(container)
 
     await user.clear(chocolateInput)
     await user.type(chocolateInput, '2')     
